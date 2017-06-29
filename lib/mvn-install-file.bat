7db9d06d6232d2ef816566cf63f9b7f9756a3eda
sgoeschl <siegfried.goeschl@it20one.at>
2017-06-29 23:18:24 +0200

    Fix broken Travis build caused case-sensitive file name

--- lib/mvn-install-file.bat
+++
@@ -1,3 +0,0 @@
-# Helper script to import libraries not found in the global Maven repo 
-
-call mvn install:install-file -Dfile=gatling-blueprint-extensions-2.2.3.7.jar -Dversion=2.2.3.7 -DgroupId=org.github.sgoeschl.gatling -DartifactId=gatling-blueprint-extensions -Dpackaging=jar -DcreateChecksum=true -DpomFile=gatling-blueprint-extensions-2.2.3.7.pom
