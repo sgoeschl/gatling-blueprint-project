f61879ead7489969559ae41938a04370cc80297e
sgoeschl <siegfried.goeschl@it20one.at>
2017-06-29 23:12:52 +0200

    Add travis configuration

--- lib/mvn-install-file.sh
+++ lib/mvn-install-file.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 # Helper script to import libraries not found in the global Maven repo 
 
-mvn install:install-file -Dfile=gatling-blueprint-extensions-2.2.3.7.jar -Dversion=2.2.3.7 -DgroupId=org.github.sgoeschl.gatling -DartifactId=gatling-blueprint-extensions -Dpackaging=jar -DcreateChecksum=true -DpomFile=gatling-blueprint-extensions-2.2.3.7.pom
+mvn install:install-file -Dfile=./lib/gatling-blueprint-extensions-2.2.3.7.jar -Dversion=2.2.3.7 -DgroupId=org.github.sgoeschl.gatling -DartifactId=gatling-blueprint-extensions -Dpackaging=jar -DcreateChecksum=true -DpomFile=./lib/gatling-blueprint-extensions-2.2.3.7.pom
