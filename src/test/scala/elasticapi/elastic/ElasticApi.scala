ed70e0d8c6c29557a3c80c72fff03c8795e865fe
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2018-08-10 19:09:25 +0200

    Improve error handling of Elastic tests

--- src/test/scala/elasticapi/elastic/ElasticApi.scala
+++ src/test/scala/elasticapi/elastic/ElasticApi.scala
@@ -7,17 +7,21 @@ import io.gatling.http.Predef._
 
 object ElasticApi {
 
+  val HTTP_OK = 200
+
   val search: ChainBuilder =
     exec(_.set("ELASTICBASEURL", ConfigurationTool.getURL("elasticapi")))
       .exec(http("${LABEL}")
         .post("${ELASTICBASEURL}/${URL}")
+        // enable if you want to pass the line number of the current request
         // .queryParam("_", "${LINENUMBER}")
         .body(StringBody("${QUERY}")).asJSON
-        .check(
-          jsonPath("$").ofType[Any].find.saveAs("lastResponse"))
-      )
+        .check(status.is(HTTP_OK))
+        .check(status.saveAs("lastResponseStatus"))
+        .check(checkIf((response: Response, session: Session) =>
+          session("lastResponseStatus").as[Int] == HTTP_OK)(jsonPath("$").ofType[Any].find.saveAs("lastResponse"))))
       .exec(session => {
-        if (ConfigurationTool.isResponseSaved) {
+        if (ConfigurationTool.isResponseSaved && session.contains("lastResponse")) {
           val jsonString = JsonResponseTool.modify(session, "lastResponse", "took")
           JsonResponseTool.saveToFile(jsonString, "lastResponse", session.get("LINENUMBER").as[String])
         }
