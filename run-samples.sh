d3b3d9541248b8c4e5412ac3faf66c83b1d08ada
Siegfried Goeschl <siegfried.goeschl@gmail.com>
2018-11-30 19:14:10 +0100

    Upgrade to Gatling 3.0.1.1

---
+++ run-samples.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Run all the samples being documented 
+
+echo "Computer Database BasicSimulation"
+mvn -Pstandalone clean package
+
+echo "Computer Database BasicSimulation"
+mvn -Dgatling.simulationClass=computerdatabase.BasicSimulation gatling:test
+echo "--- FINISHED ---"
+sleep 5
+
+echo "Computer Database AdvancedSimulationStep05"
+mvn -Dgatling.simulationClass=computerdatabase.advanced.AdvancedSimulationStep05 gatling:test
+echo "--- FINISHED ---"
+sleep 5
+
+echo "Computer Database Smoketest Blueprint"
+mvn -Dgatling.simulationClass=computerdatabase.gatling.smoketest.Test gatling:test
+echo "--- FINISHED ---"
+sleep 5
+
+echo "GitHub API Functional Blueprint"
+mvn -Dgatling.simulationClass=githubapi.github.functional.Test gatling:test
+echo "--- FINISHED ---"
+sleep 5
+
+echo "Postman API Blueprint"
+mvn -Dgatling.simulationClass=postman.Test gatling:test
+echo "--- FINISHED ---"
+sleep 5
+
+echo "PostMan API using Using Minimal POM"
+mvn -f pom-minimal.xml -Dgatling.simulationClass=postman.Test gatling:test
+echo "--- FINISHED ---"
+sleep 5
